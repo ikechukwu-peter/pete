4fb25fa986f0c2b3586f8568004d4a09baf87884
ikechukwu-peter <pitiyke@gmail.com>
2022-08-30 12:13:14 +0100

    rewrites of homepage

---
+++ components/home/Introduction.js
@@ -0,0 +1,290 @@
+import {
+  Flex,
+  Box,
+  Heading,
+  Text,
+  Link,
+  Image,
+  Button,
+  useColorModeValue,
+  DarkMode,
+  Icon,
+} from "@chakra-ui/react";
+import {
+  AiOutlineLink,
+  AiOutlineMail,
+  AiOutlineDownload,
+} from "react-icons/ai";
+import Typewriter from "typewriter-effect";
+import { FaGithub } from "react-icons/fa";
+import { ImLinkedin2 } from "react-icons/im";
+import { SiInstagram } from "react-icons/si";
+
+export default function Introduction() {
+  return (
+    <>
+      <Flex
+        bg="theme.500"
+        py={{ base: "1rem", md: "4rem" }}
+        align={{ base: "center", md: "flex-start" }}
+        justify={{ base: "center", md: "space-between" }}
+        w="100%"
+      >
+        <Box>
+          <Text
+            fontWeight={800}
+            color="theme.100"
+            mb=".2rem"
+            fontSize={[".8rem", "1rem"]}
+            textAlign={{ base: "center", md: "left" }}
+          >
+            Hello!
+          </Text>
+          <Heading
+            fontWeight={600}
+            color="theme.100"
+            mb=".1rem"
+            fontSize={["2.1rem", "2.5rem"]}
+            textAlign={{ base: "center", md: "left" }}
+          >
+            {"I'm "}
+            <Heading display={"inline"} color="theme.300">
+              Peter
+            </Heading>
+          </Heading>
+          <Box
+            fontWeight={600}
+            fontSize={["1.2rem", "2rem", "2rem", "2rem"]}
+            color="theme.400"
+            textAlign={{ base: "center", md: "left" }}
+          >
+            <Typewriter
+              options={{
+                strings: ["Web developer", "Frontend + Backend"],
+                autoStart: true,
+                loop: true,
+              }}
+            />
+          </Box>
+          <Box>
+            <Text
+              fontWeight={600}
+              color="theme.700"
+              fontSize={["1rem", "1rem", "1rem", "1.2rem"]}
+              py="1rem"
+              textAlign={{ base: "center", md: "left" }}
+            >
+              I have a passion for technology.
+            </Text>
+            <Text
+              fontWeight={600}
+              fontSize={[".6rem", ".6rem", ".8rem", "1rem"]}
+              color="theme.700"
+              textAlign={{ base: "center", md: "left" }}
+            >
+              <Link
+                color="theme.300"
+                _hover={{
+                  textDecor: "none",
+                }}
+              >
+                Know More <Icon as={AiOutlineLink} pt=".4rem" />
+              </Link>
+              about me
+            </Text>
+
+            <Flex
+              gap="1rem"
+              align={"center"}
+              color="theme.100"
+              fontWeight={700}
+              textAlign={{ base: "center", md: "left" }}
+            >
+              <Text textAlign={{ base: "center", md: "left" }}>Follow me</Text>
+              <Link
+                href="https://linkedin.com/in/peter-ikechukwu"
+                isExternal
+                _hover={{
+                  textDecor: "none",
+                }}
+                _focus={{
+                  border: "none",
+                }}
+              >
+                <ImLinkedin2 />
+              </Link>
+              <Link
+                href="https://github.com/ikechukwu-peter"
+                isExternal
+                _hover={{
+                  textDecor: "none",
+                }}
+                _focus={{
+                  border: "none",
+                }}
+              >
+                <FaGithub />
+              </Link>
+              <Link
+                href="https://instagram.com/pete_iyke"
+                isExternal
+                _hover={{
+                  textDecor: "none",
+                }}
+                _focus={{
+                  border: "none",
+                }}
+              >
+                <SiInstagram />
+              </Link>
+            </Flex>
+          </Box>
+
+          <Flex gap="1rem" mt="1rem">
+            <DarkMode>
+              <Link
+                href="mailto:ikechukwupeter1999@gmail.com"
+                isExternal
+                _hover={{
+                  textDecor: "none",
+                }}
+              >
+                <Button
+                  color="theme.100"
+                  bg={"theme.300"}
+                  textDecor="none"
+                  _hover={{
+                    bg: "purple.500",
+                  }}
+                  _focus={{
+                    bg: "purple.800",
+                    border: "none",
+                  }}
+                  _active={{
+                    bg: "purple.800",
+                    border: "none",
+                  }}
+                  boxShadow={" 2px 2px 1px 1px  #ff5403"}
+                  boxSizing="border-box"
+                  mb={["2rem", "2rem", "3rem", "4rem"]}
+                  leftIcon={<AiOutlineMail />}
+                >
+                  Email Me
+                </Button>
+              </Link>
+            </DarkMode>
+            <DarkMode>
+              <Link
+                href="/Ikechukwu Peter's Resume.pdf"
+                download="Ikechukwu_Peter_CV"
+                isExternal
+                _hover={{
+                  textDecor: "none",
+                }}
+              >
+                <Button
+                  color="theme.100"
+                  bg={"theme.500"}
+                  borderWidth="2px"
+                  borderColor={"theme.300"}
+                  boxShadow={" 1px 1px 1px  #ff5403"}
+                  textDecor="none"
+                  _hover={{
+                    bg: "theme.300",
+                  }}
+                  _focus={{
+                    bg: "theme.300",
+                    border: "none",
+                  }}
+                  _active={{
+                    bg: "theme.300",
+                    border: "none",
+                  }}
+                  mb={["2rem", "2rem", "3rem", "4rem"]}
+                  leftIcon={<AiOutlineDownload />}
+                >
+                  Resume
+                </Button>
+              </Link>
+            </DarkMode>
+          </Flex>
+        </Box>
+        <Box d={["none", "none", "flex", "flex"]} w="50%">
+          <Image
+            src="/developer.svg"
+            alt="A developer writing code"
+            w="80%"
+            h="auto"
+          ></Image>
+        </Box>
+      </Flex>
+
+      <Flex
+        bg="theme.600"
+        align={{ base: "center", md: "center" }}
+        justify={{ base: "center", md: "space-between" }}
+        w="100%"
+        p="1.5rem"
+        borderRadius={"2rem"}
+        direction={[
+          "column-reverse",
+          "column-reverse",
+          "row-reverse",
+          "row-reverse",
+        ]}
+        gap="2rem"
+      >
+        <Box
+          d="flex"
+          justifyContent="center"
+          flexDir="column"
+          w={{ base: "100%", md: "50%" }}
+        >
+          <Heading
+            fontWeight={600}
+            color={"theme.100"}
+            mb=".3rem"
+            fontSize={["1.5rem", "2rem", "2.5rem", "3rem"]}
+          >
+            50+
+          </Heading>
+          <Text
+            fontWeight={600}
+            fontSize={["1.2rem", "1.4rem", "1.4rem", "1.8rem"]}
+            color={"theme.100"}
+          >
+            completed projects
+          </Text>
+          <Text
+            fontWeight={600}
+            fontSize={["1.2rem", "1.4rem", "1.4rem", "1.8rem"]}
+            color={"theme.400"}
+          >
+            Glad to you help you!
+          </Text>
+          <Text
+            fontWeight={600}
+            fontSize={[".6rem", ".6rem", ".8rem", "1rem"]}
+            color={"theme.700"}
+          >
+            {
+              "As a web developer, technical writer and a passionate learner, I'm always on the lookout for new challenges and opportunities to further improve my knowledge and skills."
+            }
+          </Text>
+        </Box>
+        <Box
+          w={{ base: "100%", md: "50%" }}
+          d={["flex", "flex", "flex", "flex"]}
+          py="2rem"
+        >
+          <Image
+            src="/webdev.svg"
+            w={["100%", "100%", "80%", "90%"]}
+            h={["100%", "100%", "100%", "50%"]}
+            alt="A man sitting and relaxing"
+          ></Image>
+        </Box>
+      </Flex>
+    </>
+  );
+}
--- pages/index.js
+++ pages/index.js
@@ -1,29 +1,11 @@
 import Head from "next/head";
-import { Box, useColorModeValue } from "@chakra-ui/react";
-
-import Header from "../components/Header";
-import Introduction from "../components/Introduction";
-import About from "../components/About";
-import Projects from "../components/Projects";
-import Services from "../components/Services";
-import Skills from "../components/Skills";
-import Contact from "../components/Contact";
-import Footer from "../components/Footer";
+import dynamic from "next/dynamic";
+import { Box, Flex } from "@chakra-ui/react";
+const Introduction = dynamic(() => import("../components/home/Introduction"));
+const Projects = dynamic(() => import("../components/home/Projects"));
+const Services = dynamic(() => import("../components/home/Services"));
 
 export default function Home() {
-  const darkBg = useColorModeValue("gray.50", "gray.800");
-  const darkTextColor = useColorModeValue("gray.600", "whiteAlpha.900");
-
-  const logo = useColorModeValue("blackAlpha.800", "whiteAlpha.900");
-  const headBg = useColorModeValue("gray.50", "gray.800");
-  const hoverBg = useColorModeValue("blackAlpha.600", "whiteAlpha.600");
-  const hamburgerNav = useColorModeValue("white", "black");
-  const serviceCardBg = useColorModeValue("whiteAlpha.900", "gray.900");
-
-  const lightBg = useColorModeValue("whiteAlpha.50", "gray.900");
-  const lightTextColor = useColorModeValue("blackAlpha.800", "whiteAlpha.900");
-  const cardBg = useColorModeValue("gray.50", "gray.800");
-
   return (
     <>
       <Head>
@@ -31,45 +13,20 @@ export default function Home() {
         <meta name="description" content={" Ikechukwu Peter's Portfolio "} />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <Header
-        headBg={headBg}
-        logo={logo}
-        hamburgerNav={hamburgerNav}
-        hoverBg={hoverBg}
-        darkTextColor={darkTextColor}
-      />
-      <Box as="section" id="introduction">
-        <Introduction lightBg={lightBg} lightTextColor={lightTextColor} />
-      </Box>
-      <Box as="section" id="about">
-        <About darkTextColor={darkTextColor} darkBg={darkBg} />
-      </Box>
-      <Box as="section" id="project">
-        <Projects
-          lightBg={lightBg}
-          lightTextColor={lightTextColor}
-          cardBg={cardBg}
-        />
-      </Box>
-      <Box as="section" id="services">
-        <Services
-          darkTextColor={darkTextColor}
-          darkBg={darkBg}
-          serviceCardBg={serviceCardBg}
-        />
-      </Box>
-
-      <Box as="section" id="skills">
-        <Skills
-          lightBg={lightBg}
-          lightTextColor={lightTextColor}
-          cardBg={cardBg}
-        />
-      </Box>
-      <Box as="section" id="contact">
-        <Contact darkTextColor={darkTextColor} darkBg={darkBg} />
-      </Box>
-      <Footer darkBg={darkBg} />
+      <Flex
+        direction={"column"}
+        align="center"
+        justify={"center"}
+        flex="1"
+        w="100%"
+        bg="theme.500"
+      >
+        <Box w="90%" m="auto">
+          <Introduction />
+          <Projects />
+          <Services />
+        </Box>
+      </Flex>
     </>
   );
 }
