3485fafd3714f2929a401aab4d515a0c888a8dbf
ikechukwu-peter <pitiyke@gmail.com>
2022-09-01 11:03:42 +0100

    fixed preloading issues

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,7 @@
 import { useRouter } from "next/router";
 import { ChakraProvider } from "@chakra-ui/react";
 import dynamic from "next/dynamic";
-import { useEffect, useState } from "react";
+import { Suspense, useEffect, useState } from "react";
 const Header = dynamic(() => import("../components/Header"));
 const Footer = dynamic(() => import("../components/Footer"));
 import { ScrollToTop } from "../components/ScrollToTop";
@@ -12,10 +12,21 @@ import Loader from "../components/loader/Loader";
 function MyApp({ Component, pageProps }) {
   const router = useRouter();
   const [loading, setLoading] = useState(false);
+  const [pageLoad, setPageLoad] = useState(true);
 
   useEffect(() => {
-    const handleStart = (url) => url !== router.asPath && setLoading(true);
-    const handleComplete = (url) => url === router.asPath && setLoading(false);
+    setPageLoad(false);
+  }, []);
+
+  useEffect(() => {
+    const handleStart = (url) => {
+      return url !== router.asPath && setLoading(true);
+    };
+    const handleComplete = (url) => {
+      console.log(url, router.asPath);
+      // return url === router.asPath && setLoading(false);
+      setLoading(false);
+    };
     router.events.on("routeChangeStart", handleStart);
     router.events.on("routeChangeComplete", handleComplete);
     router.events.on("routeChangeError", handleComplete);
@@ -27,15 +38,20 @@ function MyApp({ Component, pageProps }) {
   }, [router.events, router.asPath]);
   return (
     <ChakraProvider theme={theme} resetCSS>
-      {loading ? (
+      {pageLoad ? (
         <Loader />
       ) : (
         <>
-          {" "}
-          <Header />
-          <Component {...pageProps} />
-          <ScrollToTop />
-          <Footer />
+          {loading ? (
+            <Loader />
+          ) : (
+            <>
+              <Header />
+              <Component {...pageProps} />
+              <ScrollToTop />
+              <Footer />
+            </>
+          )}
         </>
       )}
     </ChakraProvider>
