4cef98e03b263eaa64956a232121957c87fbf1b0
ikechukwu-peter <pitiyke@gmail.com>
2024-01-28 16:35:21 +0100

    feat: sitemap

--- src/pages/sitemap.xml.ts
+++ src/pages/sitemap.xml.ts
@@ -1,12 +1,12 @@
-// pages/sitemap.xml.js
-
-import { NextResponse } from "next/server";
-
 const homepage = process.env.NEXT_CLIENT_BASE_URL;
 
 function generateSiteMap() {
   return `<?xml version="1.0" encoding="UTF-8"?>
-    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"
+    xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+      xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9
+            http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd">
+    >
       <url>
         <loc>${homepage}</loc>
         <lastmod>${new Date().toISOString()}</lastmod>
